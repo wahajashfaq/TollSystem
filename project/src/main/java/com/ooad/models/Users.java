338dfc1ba510250bd707f74705178e36b926294d
Muhammad Bilal <yousuf-bilal1@hotmail.com>
2019-07-23 14:46:50 -0500

    create user class

---
+++ project/src/main/java/com/ooad/models/Users.java
@@ -0,0 +1,48 @@
+package com.ooad.models;
+
+public class Users {
+private String fName;
+private String LName;
+private String address;
+private int phoneNumber;
+private String dLicense;
+private String bankInfo;
+public String getfName() {
+	return fName;
+}
+public void setfName(String fName) {
+	this.fName = fName;
+}
+public String getLName() {
+	return LName;
+}
+public void setLName(String lName) {
+	LName = lName;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public int getPhoneNumber() {
+	return phoneNumber;
+}
+public void setPhoneNumber(int phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+public String getdLicense() {
+	return dLicense;
+}
+public void setdLicense(String dLicense) {
+	this.dLicense = dLicense;
+}
+public String getBankInfo() {
+	return bankInfo;
+}
+public void setBankInfo(String bankInfo) {
+	this.bankInfo = bankInfo;
+}
+
+
+}
